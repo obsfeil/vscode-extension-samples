f47f5e75d46379b457fd7add2d401ddfa9f20490
Atle Stromsvold <obsfeil@outlook.com>
2024-01-07 13:39:54 +0100

    bb

--- .base-sample/src/extension.ts
+++ .base-sample/src/extension.ts
@@ -1,8 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscon de';
+import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
+// this method is called when your /extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
